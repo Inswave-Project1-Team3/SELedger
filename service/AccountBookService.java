cdff27e088ad11fe8edb741aa6dc5e3a533f9ee8
K-IMjihun <kjh950901@gmail.com>
2025-03-11 13:27:03 +0900

    03-11 / Update: 일별 저장 코드 제거 및 월별 저장코드로 값을 불러올 수 있도록 수정

--- service/AccountBookService.java
+++ service/AccountBookService.java
@@ -7,103 +7,56 @@ import model.MonthAccountBook;
 import model.TransactionAccountBook;
 
 import java.io.*;
-import java.time.YearMonth;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static app.App.dayAccountBookMap;
-import static app.App.monthAccountBook;
-
 public class AccountBookService implements Serializable {
     private static final long serialVersionUID = 1L;
-    /**
-     * 월별 가계부 저장소
-     */
-    private Map<YearMonth, MonthAccountBook> accountBookRecords = new HashMap<>();
-
-    /**
-     * 특정 월의 가계부 가져오기 (없으면 생성)
-     *
-     * @param month 조회할 월
-     * @return 해당 월의 가계부
-     */
-    private MonthAccountBook getOrCreateMonthAccountBook(YearMonth month) {
-        return accountBookRecords.computeIfAbsent(month, MonthAccountBook::new);
-    }
-
-    /**
-     * 월별 가계부를 파일로 저장 (직렬화 방식)
-     */
-    public void saveMonthAccountBook(MonthAccountBook monthAccountBook) {
-        File file = getMonthAccountBookFile(monthAccountBook.getMonth());
-        file.getParentFile().mkdirs();
-
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-            oos.writeObject(monthAccountBook);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("파일 저장 오류: " + file.getAbsolutePath());
-        }
-    }
-
-    /**
-     * 월별 가계부를 파일에서 로드 (역직렬화 방식)
-     */
-    public MonthAccountBook loadMonthAccountBook(YearMonth month) {
-        File file = getMonthAccountBookFile(month);
-        if (!file.exists()) {
-            return new MonthAccountBook(month);
-        }
-
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            return (MonthAccountBook) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return new MonthAccountBook(month);
-        }
-    }
-
-    /**
-     * 특정 월의 가계부 파일 경로 반환
-     */
-    private File getMonthAccountBookFile(YearMonth month) {
-        return new File("data/" + month + ".ser");
-    }
-
-
 
 
     // 선택한 날의 가계부 생성
     public void createAccountBook(CreateAccountBookDTO accountBookDTO,
                                   CreateTransactionAccountBookDTO transactionAccountBookDTO,
                                   int day) {
-        getToFile();
+        int month = LocalDateTime.now().getMonthValue();
+
+        Map<Integer, Map<Integer, DayAccountBook>> monthAccountBook = getToFile();
+
+        if (!monthAccountBook.containsKey(month)) monthAccountBook.put(month, new HashMap<>());
+
+        Map<Integer, DayAccountBook> dayAccountBookMap = monthAccountBook.get(month);
 
         TransactionAccountBook transactionAccountBook = new TransactionAccountBook(
                 transactionAccountBookDTO.isBenefit(),
                 transactionAccountBookDTO.getMoney()
         );
 
-        List<TransactionAccountBook> list = (dayAccountBookMap.get(day) == null) ?
-                        new ArrayList<>() : dayAccountBookMap.get(day).getTransactionAccountBooks();
+        List<TransactionAccountBook> list;
+
+        if (!dayAccountBookMap.containsKey(day)) {
+            list = new ArrayList<>();
+        } else {
+            DayAccountBook dayAccountBook = dayAccountBookMap.get(day);
+            list = (dayAccountBook.getTransactionAccountBooks() != null) ?
+                    dayAccountBook.getTransactionAccountBooks() : new ArrayList<>();
+        }
+
         list.add(transactionAccountBook);
 
         DayAccountBook dayAccountBook = new DayAccountBook(accountBookDTO.getMemo(), list);
 
-        dayAccountBookMap.put(day, dayAccountBook);
-        monthAccountBook.put(3, dayAccountBookMap);
+        monthAccountBook.get(month).put(day, dayAccountBook);
 
-        saveToFile();
+        saveToFile(monthAccountBook);
     }
 
-
-    private void saveToFile() {
+    private void saveToFile(Map<Integer, Map<Integer, DayAccountBook>> monthAccountBook) {
         try (FileOutputStream fileOut = new FileOutputStream("C:\\Temp\\day_account_book.ser");
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
 
-            out.writeObject(dayAccountBookMap);
             out.writeObject(monthAccountBook);
             System.out.println("등록되었습니다.");
 
@@ -112,61 +65,70 @@ public class AccountBookService implements Serializable {
         }
     }
 
-    // 지정된 위치의 일일 거래내역 가져오기
-    // 저장위치 바뀌면 파라미터 값으로 전달 후 조절하면 될듯합니다
-    public void getToFile() {
+    // 지정된 위치의 거래내역 가져오기
+    public Map<Integer, Map<Integer, DayAccountBook>> getToFile() {
         File file = new File("C:\\Temp\\day_account_book.ser");
+        Map<Integer, Map<Integer, DayAccountBook>> monthAccountBook = new HashMap<>();
+
         if (!file.exists()) {
             System.out.println("거래내역이 존재하지 않습니다");
-            return;
-        }
+        } else {
+            try (FileInputStream fileIn = new FileInputStream(file);
+                 ObjectInputStream in = new ObjectInputStream(fileIn)) {
 
-        try (FileInputStream fileIn = new FileInputStream(file);
-             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                Object obj = in.readObject();
 
-            // 데이터를 읽어온 후 null 체크
-            Object obj = in.readObject();
-            dayAccountBookMap = (obj instanceof Map) ? (Map<Integer, DayAccountBook>) obj : new HashMap<>();
+                if (obj instanceof Map) monthAccountBook = (Map<Integer, Map<Integer, DayAccountBook>>) obj;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void getDayAccountBook(int number){
-        getToFile();
-
-        System.out.println("📅 " + number + "일 가계부");
-        if(!(dayAccountBookMap.get(number) == null)) {
-            for (int i = 0; i < dayAccountBookMap.get(number).getTransactionAccountBooks().size(); i++) {
-                System.out.println(dayAccountBookMap.get(number).getTransactionAccountBooks().get(i).getMoney() +
-                        ", " + dayAccountBookMap.get(number).getTransactionAccountBooks().get(i).getCreateDate() + ", " +
-                        dayAccountBookMap.get(number).getTransactionAccountBooks().get(i).isBenefit());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        System.out.println("메모내용 : " + dayAccountBookMap.get(number).getMemo());
         }
+        return monthAccountBook;
+    }
 
-        else{
-            dayAccountBookMap = new HashMap<>();
+    public void getDayAccountBook(int month, int day) {
+        Map<Integer, Map<Integer, DayAccountBook>> monthAccountBook = getToFile();
+
+        System.out.println("📅 " + day + "일 가계부");
+        DayAccountBook dayAccountBook;
+        if (monthAccountBook.containsKey(month) && monthAccountBook.get(month).containsKey(day)) {
+            dayAccountBook = monthAccountBook.get(month).get(day);
+        } else {
+            dayAccountBook = new DayAccountBook();
+        }
+        if (dayAccountBook != null) {
+            for (int i = 0; i < dayAccountBook.getTransactionAccountBooks().size(); i++) {
+                System.out.println(dayAccountBook.getTransactionAccountBooks().get(i).getMoney() +
+                        ", " + dayAccountBook.getTransactionAccountBooks().get(i).getCreateDate() + ", " +
+                        dayAccountBook.getTransactionAccountBooks().get(i).isBenefit());
+            }
+            System.out.println("메모내용 : " + dayAccountBook.getMemo());
+        } else {
+            monthAccountBook.get(month).put(day, new DayAccountBook());
         }
     }
 
-    public void getMonthAccountBook(){
-        getToFile();
-        System.out.println("📅 " + "3월 가계부");
+    public void getMonthAccountBook() {
+        Map<Integer, Map<Integer, DayAccountBook>> monthAccountBook = getToFile();
+        int currentMonth = LocalDateTime.now().getMonthValue();
+        System.out.println("📅 " + currentMonth +"월 가계부");
+
         for (Map.Entry<Integer, Map<Integer, DayAccountBook>> entry : monthAccountBook.entrySet()) {
             Integer month = entry.getKey();
-            Map<Integer, DayAccountBook> dailyAccountBook = entry.getValue();
+            Map<Integer, DayAccountBook> dayAccountBookMap = entry.getValue();  // Map<Integer, DayAccountBook> 추출
 
-            // 월별 값
-            for (Map.Entry<Integer, DayAccountBook> dayEntry : dailyAccountBook.entrySet()) {
+            System.out.println(month + "월:");
+
+            // 일별로 DayAccountBook을 출력
+            for (Map.Entry<Integer, DayAccountBook> dayEntry : dayAccountBookMap.entrySet()) {
                 Integer day = dayEntry.getKey();
-                List<TransactionAccountBook> transactionAccountBooks = dayEntry.getValue().getTransactionAccountBooks();
-                for(TransactionAccountBook transactionAccountBook : transactionAccountBooks){
-                    System.out.println("Month: " + month +
-                            ", Day: " + day +
-                            ", money: " + transactionAccountBook.getMoney() +
-                            ", benefit : " + transactionAccountBook.isBenefit());
+                DayAccountBook dayAccountBook = dayEntry.getValue();
+
+                System.out.println("  " + day + "일 메모: " + dayAccountBook.getMemo());
+                for (TransactionAccountBook transactionAccountBook : dayAccountBook.getTransactionAccountBooks()) {
+                    System.out.println("    가격: " + transactionAccountBook.getMoney());
+                    System.out.println("         수익여부: " + transactionAccountBook.isBenefit());
                 }
             }
         }
